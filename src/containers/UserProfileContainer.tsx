14205d08f3bfe0c3fd46c3dbb12a49a1214a33e4
saneyoshi <saneyoshi@collabos.com>
2023-01-05 16:52:07 +0900

    add:ユーザーページの実装:UserProfileContainerの実装

---
+++ src/containers/UserProfileContainer.tsx
@@ -0,0 +1,40 @@
+import UserProfile from 'components/organisms/UserProfile'
+import useUser from 'services/users/use-user'
+import type { ApiContext, User } from 'types'
+
+const context: ApiContext = {
+  apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
+}
+
+interface UserProfileContainerProps {
+  /**
+   * ユーザID
+   */
+  userId: number
+  /**
+   * 初期で表示するユーザ
+   */
+  user?: User
+}
+
+/**
+ * ユーザプロファイルコンテナ
+ */
+const UserProfileContainer = ({ userId, user }: UserProfileContainerProps) => {
+  // 細心のユーザ情報を取得し、更新があった場合には
+  // initialで指定されているデータを上書きする
+  const { user: u } = useUser(context, { id: userId, initial: user })
+
+  if (!u) return <div>Loading...</div>
+
+  return (
+    <UserProfile
+      username={`${u.username} (${u.displayName})`}
+      profileImageUrl={u.profileImageUrl}
+      numberOfProducts={100}
+      description={u.description}
+    />
+  )
+}
+
+export default UserProfileContainer
